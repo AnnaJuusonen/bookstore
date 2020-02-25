b995243726d5a1533ddf8e57f3af016d2637f0bb
AnnaJuusonen <annajuusonen@gmail.com>
2020-02-25 11:13:31 +0200

    JPA and database

--- bookstore/bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package hh.swd20.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save few books");
+			repository.save(new Book("Harry Potter ja viisasten kivi", "JK Rowling", 2001, "1224434", 30.00));
+			repository.save(new Book("Harry Potter ja salainen kammio", "JK Rowling", 2002, "134234234", 35.90));
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+			
+		};
 	}
 
 }
--- bookstore/bookstore/src/main/java/hh/swd20/bookstore/webcontroller/BookController.java
+++ bookstore/bookstore/src/main/java/hh/swd20/bookstore/webcontroller/BookController.java
@@ -1,14 +1,23 @@
 package hh.swd20.bookstore.webcontroller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
 	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String getBooks() {
+		List<Book> books = repository.findByTitle("Harry Potter");
 	return "frontpage";	
 	}
 
